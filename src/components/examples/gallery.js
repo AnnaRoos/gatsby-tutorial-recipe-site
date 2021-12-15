ab144d521d13a30bc0c4b9c9083c85db58e4cdf3
Anna Roos <anna.flytstrom@gmail.com>
2021-12-15 15:40:09 +0100

    tried out GatsbyImage component, started with headless cms

--- src/components/examples/gallery.js
+++ src/components/examples/gallery.js
@@ -1,11 +1,63 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import styled from "styled-components"
+
+const query = graphql`
+  {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: BLURRED
+            transformOptions: { grayscale: true }
+            width: 200
+            height: 200
+          )
+        }
+      }
+    }
+  }
+`
 
 const Gallery = () => {
+  const data = useStaticQuery(query)
+  const nodes = data.allFile.nodes
   return (
-    <div>
-      <h2>Simple image gallery</h2>
-    </div>
+    <Wrapper>
+      <h2 className="heading">Simple image gallery</h2>
+      {nodes.map((image, index) => {
+        const { childImageSharp, name } = image
+        const pathToIamge = getImage(image)
+        return (
+          <article key={index} className="item">
+            <GatsbyImage
+              image={pathToIamge}
+              alt={name}
+              className="gallery-img"
+            />
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    margin-right: 1rem;
+  }
+  .gallery-img {
+    border-radius: 1rem;
+  }
+  .heading {
+    width: 100vw;
+  }
+`
 
 export default Gallery
