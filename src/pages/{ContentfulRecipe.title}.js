b83dc2974a01bde37a3817501caf1018d25ef0fc
Anna Roos <anna.flytstrom@gmail.com>
2021-12-20 13:44:51 +0100

    finalizing racipe pages

---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,107 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs"
+import Layout from "../components/Layout"
+
+const RecipeTemplate = ({ data }) => {
+  const {
+    title,
+    id,
+    cookTime,
+    prepTime,
+    content,
+    description,
+    servings,
+    image,
+  } = data.contentfulRecipe
+  const pathToImage = getImage(image)
+  const { ingredients, instructions, tags, tools } = content
+  return (
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-img"
+            />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>Prep time</h5>
+                  <p>{prepTime} min</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>Cook time</h5>
+                  <p>{cookTime} min</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>Servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              <p className="recipe-tags">
+                Tags :
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+          <section className="recipe-content">
+            <article>
+              <h4>Instructions</h4>
+              {instructions.map((item, index) => {
+                return (
+                  <div key={index} className="single-instruction">
+                    <header>
+                      <p>step {index + 1}</p>
+                      <div></div>
+                    </header>
+                    <p>{item}</p>
+                  </div>
+                )
+              })}
+            </article>
+            <article className="second-column"></article>
+          </section>
+        </div>
+      </main>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      id
+      prepTime
+      cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
+
+export default RecipeTemplate
